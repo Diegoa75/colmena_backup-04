d5d69b789390125f4d630bc9e87125f664880052
Diego <diegoalerei@gmail.com>
2017-11-10 20:58:34 -0300

    Home alumno

---
+++ proyecto-limpio-spring/src/main/java/ar/edu/unlam/tallerweb1/dao/Preguntas/PreguntaDao.java
@@ -0,0 +1,13 @@
+package ar.edu.unlam.tallerweb1.dao.Preguntas;
+
+import java.util.ArrayList;
+
+import ar.edu.unlam.tallerweb1.modelo.Pregunta;
+
+
+public interface PreguntaDao {
+	
+	public void grabarPregunta(Pregunta Pregunta);	
+
+	
+}
---
+++ proyecto-limpio-spring/src/main/java/ar/edu/unlam/tallerweb1/servicios/Preguntas/ServicioPregunta.java
@@ -0,0 +1,11 @@
+package ar.edu.unlam.tallerweb1.servicios.Preguntas;
+
+
+import ar.edu.unlam.tallerweb1.modelo.Pregunta;
+
+public interface ServicioPregunta {
+
+	public void grabarPregunta(Pregunta Pregunta);
+	
+	
+}
